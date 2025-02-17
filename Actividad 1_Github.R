1f5e047c0c35f5e884a6d9fecaaf2ecc3564be50
Nicolas Aristizabal <n.aristizabala@uniandes.edu.co>
2025-02-17 16:48:20 -0500

    puse mi nombre

--- Actividad 1_Github.R
+++ Actividad 1_Github.R
@@ -13,9 +13,13 @@ brauni
 # Ana Sofía Sánchez
 # amO tidyverse
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 #Alejo mostacho
 # Nicolas Aristizabal
 
+=======
+# Nicolas Aristizabal
+>>>>>>> Stashed changes
 =======
 # Nicolas Aristizabal
 >>>>>>> Stashed changes

84b9f986ccded737b91726710f8114a6a8688f32
Nicolas Aristizabal <n.aristizabala@uniandes.edu.co>
2025-04-12 18:15:36 -0500

    Primer avance
    Convertí las bases de datos en formato .dta para que pudieran ser leídas en R. Luego, realicé los estadísticos descriptivos de las variables que miden la compra de votos y, a su vez, generé una tabla de correlación entre esas mismas variables.

--- Codigo/Exploracion1.R
+++ Codigo/Exploracion1.R
@@ -1,6 +1,23 @@
+#Ruta de acceso para los datos
 setwd("C:/Users/nikko/OneDrive/Documents/Semillero R/CAPACIDAD-ESTATAL/Codigo")
 
+#instalacion de paquetes necesarios
 install.packages("haven")
-
 library(haven)
 
+#leer datos .dta en R
+datos_c <- read_dta("C:/Users/nikko/OneDrive/Documents/Semillero R/CAPACIDAD-ESTATAL/Paquete Rep. Nat/Final/main_data_candidate_level.dta")
+datos_m <- read_dta("C:/Users/nikko/OneDrive/Documents/Semillero R/CAPACIDAD-ESTATAL/Paquete Rep. Nat/Final/main_data_municipal_level.dta")
+
+#Estadisticos descriptivos de las variables medidas para compra de votos
+summary(datos_m %>% 
+          select(starts_with("z_likelihood_misdeed_")))
+
+#Tabla de correlaciones entre las variables medidas para compra de votos
+correlaciones <- cor(datos_m[, c("z_likelihood_misdeed_1", 
+                               "z_likelihood_misdeed_2",
+                               "z_likelihood_misdeed_3",
+                               "z_likelihood_misdeed_4",
+                               "z_likelihood_misdeed_5",
+                               "z_likelihood_misdeed_6")],
+                     use = "complete.obs")  

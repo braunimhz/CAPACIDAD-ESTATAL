0090cd779acf6eee88bead125dc3eb1b9a293dc2
Nicolas Aristizabal <n.aristizabala@uniandes.edu.co>
2025-05-08 16:35:53 -0500

    bases acopladas

---
+++ Codigo/Bases acopladas.R
@@ -0,0 +1,10 @@
+setwd("C:/Users/nikko/OneDrive/Documents/Semillero R/CAPACIDAD-ESTATAL/Codigo")
+
+library(haven)
+library(dplyr)
+
+panel <- read_dta("C:/Users/nikko/OneDrive/Documents/Semillero R/CAPACIDAD-ESTATAL/PANEL_BUEN_GOBIERNO(2024).dta")
+datos_m <- read_dta("C:/Users/nikko/OneDrive/Documents/Semillero R/CAPACIDAD-ESTATAL/Paquete Rep. Nat/Final/main_data_municipal_level.dta")
+
+
+datos_unidos <- full_join(panel, datos_m, by = c("codmpio" = "ID"))
